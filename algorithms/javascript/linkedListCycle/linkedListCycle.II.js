9699fdbd779cc4541448e1019f9c57159441ca31
catcoolion <catcoolion@hotmail.com>
2024-09-28 20:22:08 +0800

    new problem solution - "142. Linked List Cycle II"

---
+++ algorithms/javascript/linkedListCycle/linkedListCycle.II.js
@@ -0,0 +1,49 @@
+/**********************************************************************************
+ * 142. Given the head of a linked list, return the node where the cycle begins. 
+ * 
+ * If there is no cycle, return null.
+ * 
+ * 
+ * 
+ * Whether there are cycle
+ * 
+ * has cycle, find the cycle begins
+ * 
+ * 
+ * 
+ *  If slow pointer moves n steps to the cycle begins, at this time, fast pointer moves 2n steps
+ * 
+ *  If cycle's length is x
+ * 
+ *  Fast pointer distance the cycle begins: x - n
+ * 
+ *  Fast pointer needs to be moved n steps to cycle begins
+ * 
+ *  Slow pointer back to start position
+ * 
+ *  Let them moves n steps together to the cycle begins
+ * 
+*/
+
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+const detectCycle = head => {
+    if (head === null || head.next === null) return null
+    let p = q = head
+    do {
+        p = p.next
+        q = q.next.next
+    } while (q && q.next && p !== q)
+    if (q === null || q.next === null) return null
+    // has cycle
+    p = head
+    while (p !== q) p = p.next, q = q.next
+    return p
+}
